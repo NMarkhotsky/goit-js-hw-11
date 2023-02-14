08e72a9ac6f9278a9599a61c94b7f602799f3d77
NMarkhotsky <111852661+NMarkhotsky@users.noreply.github.com>
2023-02-14 15:34:00 +0000

    Deploying to gh-pages from @ NMarkhotsky/goit-js-hw-11@f3bd4357dac7fb847b6bbfeb66bf819bea6767f2 🚀

---
+++ index.683e1d8c.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=33634172-69812b587cbe0ba586ff0443e&q=flowers").then((function(e){return e.json()})).then((function(e){var n=e.hits;return console.log(n)}));
+//# sourceMappingURL=index.683e1d8c.js.map
